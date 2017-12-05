788ebbf4f51040e6f92c307689fd54a0ea64b8b2
Connor Rigby <connor@farmbot.io>
2017-12-04 20:31:49 -0800

    Deploy pls

--- lib/farmbot_slackbot/application.ex
+++ lib/farmbot_slackbot/application.ex
@@ -1,11 +1,13 @@
 defmodule FarmbotSlackbot.Application do
   use Application
+  require Logger
 
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec
     install_nerves_bootstrap()
+    # install_fwup()
 
     # Define workers and child supervisors to be supervised
     children = [
@@ -32,9 +34,21 @@ defmodule FarmbotSlackbot.Application do
 
   def install_nerves_bootstrap do
     unless File.exists?("bootstrap_installed") do
+      Logger.debug "installing nerves"
       Mix.Tasks.Archive.Install.run ["hex", "nerves_bootstrap", "--force"]
       File.write!("bootstrap_installed", "OK")
     end
     :ok
   end
+
+  # @fwup_dl_url "https://github.com/fhunleth/fwup/releases/download/v0.18.1/fwup_0.18.1_amd64.deb" |> to_charlist()
+  #
+  # def install_fwup do
+  #   unless File.exists?("fwup_installed!") do
+  #     Logger.debug "installing fwup"
+  #     {:ok, {{_, 200, _}, _, bin}} = :httpc.request(:get, {@fwup_dl_url, []}, [{:autoredirect, true}, {:body_format, :binary}], [{:autoredirect, true}, {:body_format, :binary}])
+  #     File.write!("/tmp/fwup_0.18.1_amd64.deb", bin)
+  #     require IEx; IEx.pry
+  #   end
+  # end
 end
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule FarmbotSlackbot.Mixfile do
   def application do
     [
       mod: {FarmbotSlackbot.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :crypto, :inets]
     ]
   end
 
