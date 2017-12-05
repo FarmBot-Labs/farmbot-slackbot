34a88ec70fd12da098117908390a96d30bf17439
Connor Rigby <connor@farmbot.io>
2017-12-04 16:47:42 -0800

    Test deploy

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :farmbot_slackbot, FarmbotSlackbotWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "lit-badlands-37583.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -57,8 +58,3 @@ config :logger, level: :info
 # start per endpoint:
 #
 #     config :farmbot_slackbot, FarmbotSlackbotWeb.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
--- lib/farmbot_slackbot_web/channels/user_socket.ex
+++ lib/farmbot_slackbot_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule FarmbotSlackbotWeb.UserSocket do
   # channel "room:*", FarmbotSlackbotWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
